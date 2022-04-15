710481b8869d0e5aa7b99b1b8a3e1c8643ec8246
zzacal <jizacal@gmail.com>
2022-04-15 09:41:07 -0700

    Added js airplanes

---
+++ AirplanesJs/airplane-service.js
@@ -0,0 +1,5 @@
+export class AirplaneService {
+  save(airplane) {}
+
+  get(id) {}
+}
---
+++ AirplanesJs/index.js
@@ -0,0 +1,7 @@
+import { AirplaneService } from "./airplane-service";
+
+const planes = new AirplaneService();
+
+const airplane1 = {};
+
+planes.save(airplane1);
