ed3d648c805e2ea554235c630ae3b8f1c75c7663
zzacal <jizacal@gmail.com>
2022-05-23 13:54:03 -0700

    Improve js

--- AirplanesJs/index.js
+++ AirplanesJs/index.js
@@ -1,7 +1,14 @@
-import { AirplaneService } from "./airplane-service";
+import { AirplaneService } from "./airplane-service.js";
 
 const planes = new AirplaneService();
 
-const airplane1 = {};
+const airplane1 = {
+  id: 0,
+  name: "Major Lift",
+  seats: 200,
+};
 
 planes.save(airplane1);
+const result = planes.get(airplane1.id);
+
+console.log(result.id);
